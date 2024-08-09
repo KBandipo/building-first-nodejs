38fb530ce82281246b78a172f85d275fd85784f2
KBandipo~ <keneebandipo@gmail.com>
2024-08-09 23:06:39 +0100

    Create get request function

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+var post = [
+  { id: '1', day: 'Monday', task: 'Learn React' },
+  { id: '2', day: 'Tuesday', task: 'Learn Next.js' },
+  { id: '3', day: 'Wednesday', task: 'Learn Node.js' },
+  { id: '4', day: 'Thursday', task: 'Learn SASS' },
+];
+
+app.get('/', function (request, response) {
+  response.send(post);
+});
+
+app.listen(3030, function () {
+  console.log('First API running on port 3030!');
+});
