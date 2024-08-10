88eafd6d67b1c56a0981bd0d9fb1d1f7e81ec4a2
KBandipo~ <keneebandipo@gmail.com>
2024-08-10 11:53:13 +0100

    Handle post request

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var post = [
+var posts = [
   { id: '1', day: 'Monday', task: 'Learn React' },
   { id: '2', day: 'Tuesday', task: 'Learn Next.js' },
   { id: '3', day: 'Wednesday', task: 'Learn Node.js' },
@@ -13,7 +13,19 @@ var post = [
 ];
 
 app.get('/', function (request, response) {
-  response.send(post);
+  response.send(posts);
+});
+
+app.post('/post', function (request, response) {
+  var post = request.body;
+  if (!post || post.day === '' || post.task === '') {
+    response
+      .status(500)
+      .send({ error: "You can't send an empty text for the day and task" });
+  } else {
+    posts.push(post);
+    response.status(200).send(post);
+  }
 });
 
 app.listen(3030, function () {
